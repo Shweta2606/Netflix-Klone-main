ed69aa40714242592a0f4275c68e3c43a775a96c
Shweta2606 <shwetayadav9625@gmail.com>
2023-10-02 15:10:02 +0530

    first commit

---
+++ Netflix-Klone-main/src/App.js
@@ -0,0 +1,29 @@
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import { AuthContextProvider } from "./context/AuthContext";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Account from "./pages/Account";
+import ProtectedRoute from "./components/ProtectedRoute";
+import { ToastContainer } from 'react-toastify';
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <AuthContextProvider>
+        <Navbar />
+        <ToastContainer />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/account" element={<ProtectedRoute> <Account /> </ProtectedRoute>} />
+        </Routes>
+      </AuthContextProvider>
+    </BrowserRouter>
+  );
+}
+
+export default App;
